990a1d0ce910bd777646072a8e9c7c568ecc523a
WangXiaoDong <514587649@qq.com>
2017-11-21 16:35:12 +0800

    Initial Commit

---
+++ TrainingDataPersistence20171121/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrainingDataPersistence20171121
+//
+//  Created by wxd on 2017/11/21.
+//  Copyright © 2017年 wxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TrainingDataPersistence20171121/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrainingDataPersistence20171121
+//
+//  Created by wxd on 2017/11/21.
+//  Copyright © 2017年 wxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
